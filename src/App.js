7d5f5144314b9dcb73b3db86f3a8d0a10c11bb46
Hcmwebs <64218603+Hcmwebs@users.noreply.github.com>
2020-12-24 23:22:59 +1300

    created and uploaded the project files
    Signed-off-by: Hcmwebs <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+
 import './App.css';
 
 function App() {
   const title = 'Hcmwebs Blog';
-  const hello = 'Hi! Welcome to my new blog';
-  const img = 'https://res.cloudinary.com/duymkqhnm/image/upload/v1608460164/hcmwebs/hcmwebs/IMG_3327_l9pp8q.jpg';
-  const myGithub = 'https://github.com/Hcmwebs'
-  const width = 200;
-  const height = 200;
-
-
-
+  const caption = 'Hi, I am Heze';
+  const web  = 'https://github.com/Hcmwebs';
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
-        <h2>{hello}</h2>
-        <img src={img} alt="Profile image" width  = {width}px height = {height}px/>
-        <a href={myGithub} target="_blank" rel="noopener noreferrer">View my profile</a>
+        <div className="card">
+          <div className="card__header">
+            <img src="https://res.cloudinary.com/duymkqhnm/image/upload/v1608460164/hcmwebs/hcmwebs/IMG_3327_l9pp8q.jpg" alt=" Profile image"/>
+          </div>
+          <h2>{caption}</h2>
+          <div className="card__body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga illo itaque in sint vel fugit.</p>
+          </div>
+          <a href={web} target="_blank" rel="noopener noreferrer">More about me</a>
 
 
+        </div>
       </div>
+
     </div>
   );
 }
