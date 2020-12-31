dfcf27d948164854e37f13dced5077123e67c965
Hcmwebs <64218603+Hcmwebs@users.noreply.github.com>
2021-01-01 03:18:47 +1300

    updated hooks, eventlisteners as well styles
    Signed-off-by: Hcmwebs <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home'
 
 function App() {
-  const title = 'Hcmwebs Blog';
   const caption = 'Hi, I am Heze';
   const web  = 'https://github.com/Hcmwebs';
+
   return (
     <div className="App">
       <Navbar />
@@ -14,7 +13,7 @@ function App() {
         <Home />
         <div className="card">
           <div className="card__header">
-            <img src="https://res.cloudinary.com/duymkqhnm/image/upload/v1608460164/hcmwebs/hcmwebs/IMG_3327_l9pp8q.jpg" alt=" Profile image"/>
+            <img src="https://res.cloudinary.com/duymkqhnm/image/upload/v1608460164/hcmwebs/hcmwebs/IMG_3327_l9pp8q.jpg" alt=" Profile"/>
           </div>
           <h2>{caption}</h2>
           <div className="card__body">
